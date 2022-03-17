d82412573e6232dc395f23f6f493711eedbcd70a
iankaguer <36267404+iankaguer@users.noreply.github.com>
2022-03-17 16:47:38 +0100

    merge all

--- tpNode/app.js
+++ tpNode/app.js
@@ -1,6 +1,6 @@
 const contactModel = require('./model/Contact')
 const express = require('express') //export du module express en vue de creer un serveur
-
+const cors = require('cors');
 const app = express() //creation d'une instance express
 
 const contactController = require('./controller/ContactController')
@@ -10,9 +10,9 @@ let router = express.Router()
 
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.json()) //parser en json)
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors({ origin: 'http://127.0.0.1:8000/' }))
+app.use(bodyParser.json())//parser en json)
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
 
 app.use((req, res, next) => {
     console.log('request url:', req.url)
@@ -20,31 +20,44 @@ app.use((req, res, next) => {
 
 })
 
-router.get('/contacts', function(req, res) {
-
+router.get('/contacts', function (req, res){
+    console.log(req)
     contactController.getContactList(req, res)
 })
-router.get('/contact', function(req, res) {
+router.get('/contact', function (req, res){
     contactController.getContact(req, res)
 })
-router.post('/contact', function(req, res) {
-    console.log(req.body)
+router.post('/contact', function (req, res){
     contactController.addContact(req, res)
 })
-router.put('/contact', function(req, res) {
+router.put('/contact', function (req, res){
     contactController.updateContact(req, res)
 })
-router.delete('/contact', function(req, res) {
+router.delete('/contact', function (req, res){
     contactController.deleteContact(req, res)
 })
 
-router.post('/signup', function(req, res) {
+router.post('/signup', function (req, res){
     userController.addUser(req, res)
 })
 
-router.post('/login', function(req, res) {
+router.post('/login', function (req, res){
     userController.login(req, res)
 })
+
+router.post('/user/add_contact', function (req, res){
+    userController.addUserContact(req, res)
+})
+router.get('/user', function (req, res){
+    userController.getUser(req, res)
+})
+router.delete('/user/remove_contact', function (req, res){
+    userController.deleteUserContact(req, res)
+})
+/*
+router.delete('/user/contact', function (req, res){
+    userController.deleteUserContacts(req, res)
+})*/
 
 
 
@@ -66,6 +79,6 @@ router.post('/login', function(req, res) {
 
 app.use("/api", router)
 
-app.listen(8800, () => {
+app.listen(8800, ()=>{
     console.log("working 😊😊😊")
 })
